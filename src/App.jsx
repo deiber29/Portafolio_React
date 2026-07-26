6ac6e8b6a73cffa853ca87216d1fab76bdd71483
Deiber Teran <deiberteran0204@gmail.com>
2026-07-26 15:46:52 -0400

    Se arreglo ruta components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Header from "./components/Header";
 import Biography from "./components/Biography";
-import RedesSociales from "./Components/RedesSociales";
-import DatosPersonales from "./Components/DatosPersonales";
-import Habilidades from "./Components/Habilidades";
+import RedesSociales from "./components/RedesSociales";
+import DatosPersonales from "./components/DatosPersonales";
+import Habilidades from "./components/Habilidades";
 //import Conexion from "./Components/Conexion";
-import Login from "./Components/Login";
+import Login from "./components/Login";
 import { useState } from "react";
 
 function App() {
